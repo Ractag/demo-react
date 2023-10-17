fd26059edc2a0bda591337c41783409b27dc8ef8
Hepsox <marie.delaire@outlook.com>
2023-10-17 14:46:33 +0200

    promo

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
-function Promo({finalPrice}) {
-    const [clicked, setClicked] = useState(false);
-    const [total, setTotal] = useState(finalPrice)
+function Promo({ finalPrice }) {
+  const [clicked, setClicked] = useState(false);
+  const [total, setTotal] = useState(undefined);
 
-    const handlePrice = () => {
-        if (clicked) {
-            return
-        }
-
-        setTotal((total * 0.9).toFixed(2))
-        setClicked(true);
+  const handlePrice = () => {
+    if (clicked) {
+      return;
     }
+
+    setTotal((finalPrice * 0.9).toFixed(2));
+    setClicked(true);
+    finalPrice = (total * 0.9).toFixed(2);
+  };
   return (
     <div>
-      <button disabled={clicked} onClick={handlePrice}>Promo Wilders</button>
+      <button disabled={clicked} onClick={handlePrice}>
+        Promo Wilders
+      </button>
 
-      <h3>{total}</h3>
+      <h3>{total !== undefined ? total : finalPrice}</h3>
     </div>
   );
 }
